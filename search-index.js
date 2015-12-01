f46d68faca85ebbfb06ddd53404cab0d250ea681
Pete Gadomski <pete.gadomski@gmail.com>
2015-12-01 15:17:59 +0000

    rebuild pages at 8e28dbe

--- search-index.js
+++ search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['utm'] = {"items":[[0,"","utm","Micro-library for converting from geodetic to UTM coordinates.",null,null],[3,"Ellipsoid","","",null,null],[3,"Degrees","","Degrees.",null,null],[3,"Radians","","Radians.",null,null],[5,"to_utm_wgs84","","Convert a latitude and longitude to UTM coordinates using the WGS84 ellipsoid.",null,null],[11,"as_radians","","Converts degreees to radians.",0,{"inputs":[{"name":"degrees"}],"output":{"name":"radians"}}]],"paths":[[3,"Degrees"]]};
+searchIndex['utm'] = {"items":[[0,"","utm","Micro-library for converting from geodetic to UTM coordinates.",null,null],[3,"Ellipsoid","","",null,null],[5,"to_utm_wgs84","","Converts a latitude and longitude in decimal degrees to UTM coordinates using the WGS84 ellipsoid.",null,null],[5,"radians_to_utm_wgs64","","Converts a latitude and longitude in radians to UTM coordinates using the WGS84 ellipsoid.",null,null]],"paths":[]};
 initSearch(searchIndex);
